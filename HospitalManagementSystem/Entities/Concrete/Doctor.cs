4aebfaf9a85a4b8418aad79494cf1bf103e605ee
Mustafa Harun Türkmenoğlu <75381086+mharunturkmenoglu@users.noreply.github.com>
2022-06-28 16:46:04 +0300

    Add Entities Doctor, Patient, Appointment

---
+++ HospitalManagementSystem/Entities/Concrete/Doctor.cs
@@ -0,0 +1,13 @@
+﻿using HospitalManagementSystem.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Entities.Concrete
+{
+    public class Doctor : EntityBase
+    {
+    }
+}
