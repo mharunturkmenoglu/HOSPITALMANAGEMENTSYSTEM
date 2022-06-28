fcaa7da63ada4f1127c0fa5fbf0bd6075cb01a60
Mustafa Harun Türkmenoğlu <75381086+mharunturkmenoglu@users.noreply.github.com>
2022-06-28 20:50:00 +0300

    Add generic class EfEntityRepository

---
+++ HospitalManagementSystem/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,76 @@
+﻿using HospitalManagementSystem.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HospitalManagementSystem.Data.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity> : IEntityRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        protected readonly DbContext _context;
+
+        public EfEntityRepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            _context.Set<TEntity>().AddAsync(entity);
+            return entity;
+        }
+
+        public bool Any(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().Any(predicate);
+        }
+
+        public int Count(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            return (predicate == null ? _context.Set<TEntity>().Count() : _context.Set<TEntity>().Count(predicate));
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public IList<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return query.ToList();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            query = query.Where(predicate);
+
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return query.SingleOrDefault();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            return entity;
+        }
+    }
+}
---
+++ HospitalManagementSystem/Entities/Abstract/IEntityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace HospitalManagementSystem.Entities.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        T Get(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        IList<T> GetAll(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+        bool Any(Expression<Func<T, bool>> predicate);
+        int Count(Expression<Func<T, bool>> predicate = null);
+    }
+}
