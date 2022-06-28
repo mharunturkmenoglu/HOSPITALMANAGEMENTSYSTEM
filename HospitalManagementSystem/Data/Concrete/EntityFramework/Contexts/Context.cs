26ebd6c50ee48821a3314d3afed8bd184db4735a
Mustafa Harun Türkmenoğlu <75381086+mharunturkmenoglu@users.noreply.github.com>
2022-06-28 17:49:52 +0300

    Add Ef Core Context

---
+++ HospitalManagementSystem/Data/Concrete/EntityFramework/Contexts/Context.cs
@@ -0,0 +1,15 @@
+﻿using HospitalManagementSystem.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagementSystem.Data.Concrete.EntityFramework.Contexts
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<Patient>? Patients { get; set; }
+        public DbSet<Doctor>? Doctors { get; set; }
+        public DbSet<Appointment>? Appointments { get; set; }
+    }
+}
--- HospitalManagementSystem/Program.cs
+++ HospitalManagementSystem/Program.cs
@@ -1,3 +1,6 @@
+using HospitalManagementSystem.Data.Concrete.EntityFramework.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("LocalDB");
+builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
