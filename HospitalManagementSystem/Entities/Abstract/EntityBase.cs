7818e86029f41968205f6a1db91b8f37dfd984a2
Mustafa Harun Türkmenoğlu <75381086+mharunturkmenoglu@users.noreply.github.com>
2022-06-28 16:46:43 +0300

    Add EntityBase

---
+++ HospitalManagementSystem/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual int No { get; set; }
+        public virtual string? Name { get; set; }
+        public virtual string? Surname { get; set; }
+        public virtual string? Address { get; set; }
+    }
+}
