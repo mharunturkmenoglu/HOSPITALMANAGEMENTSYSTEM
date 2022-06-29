af9cb327a3ff5702cb58b59a37c8bc932817b51d
Mustafa Harun Türkmenoğlu <75381086+mharunturkmenoglu@users.noreply.github.com>
2022-06-29 17:40:54 +0300

    feature: add doctorUpdateDto and doctorAddDto

---
+++ HospitalManagementSystem/Entities/Dtos/DoctorUpdateDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalManagementSystem.Entities.Dtos
+{
+    public class DoctorUpdateDto
+    {
+        [DisplayName("Doctor No")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [Range(1000, 9999, ErrorMessage = "{0} value must be between 1000 to 9999")]
+        public int No { get; set; }
+        [DisplayName("Doctor Name")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [MaxLength(50, ErrorMessage = "{0} field must be at least {1} characters")]
+        [MinLength(2, ErrorMessage = "{0} field must be maximum {1} characters")]
+        public string? Name { get; set; }
+        [DisplayName("Doctor Surname")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [MaxLength(50, ErrorMessage = "{0} field must be at least {1} characters")]
+        [MinLength(2, ErrorMessage = "{0} field must be maximum {1} characters")]
+        public string? Surname { get; set; }
+        [DisplayName("Doctor Surname")]
+        [Required(ErrorMessage = "{0} is required.")]
+        [MaxLength(50, ErrorMessage = "{0} field must be at least {1} characters")]
+        [MinLength(2, ErrorMessage = "{0} field must be maximum {1} characters")]
+        public string? Address { get; set; }
+    }
+}
