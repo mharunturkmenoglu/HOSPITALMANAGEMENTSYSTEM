e40b9bace194643fa85649d9cb3a0e49fff58450
Mustafa Harun Türkmenoğlu <75381086+mharunturkmenoglu@users.noreply.github.com>
2022-06-29 17:42:52 +0300

    feature : add doctor manager

---
+++ HospitalManagementSystem/Services/Abstract/IDoctorService.cs
@@ -0,0 +1,13 @@
+﻿using HospitalManagementSystem.Entities.Concrete;
+using HospitalManagementSystem.Entities.Dtos;
+
+namespace HospitalManagementSystem.Services.Abstract
+{
+    public interface IDoctorService
+    {
+        Task<Doctor> GetDoctor(int id);
+        Task AddDoctor(DoctorAddDto doctorAddDto);
+        Task UpdateDoctor(DoctorUpdateDto doctorUpdateDto, int id);
+        Task RemoveDoctor(Doctor doctor);
+    }
+}
---
+++ HospitalManagementSystem/Services/Concrete/DoctorManager.cs
@@ -0,0 +1,56 @@
+﻿using HospitalManagementSystem.Data.Abstract;
+using HospitalManagementSystem.Entities.Concrete;
+using HospitalManagementSystem.Entities.Dtos;
+using HospitalManagementSystem.Services.Abstract;
+
+namespace HospitalManagementSystem.Services.Concrete
+{
+    public class DoctorManager : IDoctorService
+    {
+        private readonly IUnitOfWork _unitofWork;
+
+        public DoctorManager(IUnitOfWork unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+
+        public async Task AddDoctor(DoctorAddDto doctorAddDto)
+        {
+            await _unitofWork.Doctors.AddAsync(new Doctor 
+            {
+                Address = doctorAddDto.Address,
+                Name = doctorAddDto.Name,
+                No = doctorAddDto.No,
+                Surname=doctorAddDto.Surname}
+            );
+            await _unitofWork.SaveAsync();
+        }
+
+        public async Task<Doctor> GetDoctor(int id)
+        {
+            var doctor = await _unitofWork.Doctors.GetAsync(x => x.Id == id);
+            return doctor;
+        }
+
+        public async Task RemoveDoctor(Doctor doctor)
+        {
+            await _unitofWork.Doctors.DeleteAsync(doctor);
+            await _unitofWork.SaveAsync();
+        }
+
+        public async Task UpdateDoctor(DoctorUpdateDto doctorUpdateDto, int id)
+        {
+            id = 6;
+            var doctor = new Doctor
+            {
+                Id = id,
+                Address = doctorUpdateDto.Address,
+                Name = doctorUpdateDto.Name,
+                No = doctorUpdateDto.No,
+                Surname = doctorUpdateDto.Surname
+            };
+            await _unitofWork.Doctors.UpdateAsync(doctor);
+            await _unitofWork.SaveAsync();
+        }
+    }
+}
